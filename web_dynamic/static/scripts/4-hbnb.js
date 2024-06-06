29ceaf1a791d50de14411ca278401f5478f964e3
Magnus <nharnharyhaw984@gmail.com>
2024-06-06 07:34:39 +0000

    Worked on the filter

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,4 +1,4 @@
-document.ready(function () {
+$(function () {
 	const amenities = {};
 	$("li input[type=checkbox]").change(function () {
 		if (this.checked) {
@@ -10,7 +10,7 @@ document.ready(function () {
 	});
 
 	// get status of API
-	$.getJSON("http://0.0.0.0:5001/api/v1/status/", (data) => {
+	$.getJSON("http://127.0.0.1:5001/api/v1/status/", (data) => {
 		if (data.status === "OK") {
 			$("div#api_status").addClass("available");
 		} else {
@@ -19,42 +19,42 @@ document.ready(function () {
 	});
 
 	// fetch data about places
-	$.post({
-		url: `${HOST}/api/v1/places_search`,
-		data: JSON.stringify({}),
-		headers: {
-			"Content-Type": "application/json",
-		},
-		success: (data) => {
-			data.forEach((place) =>
-				$("section.places").append(
-					`<article>
-			<div class="title_box">
-			<h2>${place.name}</h2>
-			<div class="price_by_night">$${place.price_by_night}</div>
-			</div>
-			<div class="information">
-			<div class="max_guest">${place.max_guest} Guest${
-						place.max_guest !== 1 ? "s" : ""
-					}</div>
-			<div class="number_rooms">${place.number_rooms} Bedroom${
-						place.number_rooms !== 1 ? "s" : ""
-					}</div>
-			<div class="number_bathrooms">${place.number_bathrooms} Bathroom${
-						place.number_bathrooms !== 1 ? "s" : ""
-					}</div>
-			</div> 
-			<div class="description">
-			${place.description}
-			</div>
-				</article>`
-				)
-			);
-		},
-		dataType: "json",
-	});
+	function searchPlace () {
+	  $.ajax({
+            type: 'POST',
+            url: 'http://127.0.0.1:5001/api/v1/places_search/',
+            contentType: 'application/json',
+            data: JSON.stringify({ amenities: Object.keys(amenities) })
+          }).done(function (data) {
+            $('section.places').empty();
+            //$('section.places').append('<h1>Places</h1>');
+            for (const place of data) {
+            const template = `<article>
+	    <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+	    <div class="max_guest">${place.max_guest} Guest${
+                                    place.max_guest !== 1 ? "s" : ""
+                            }</div>
+            <div class="number_rooms">${place.number_rooms} Bedroom${
+                                    place.number_rooms !== 1 ? "s" : ""
+                            }</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${
+                                    place.number_bathrooms !== 1 ? "s" : ""
+                            }</div>
+            </div>
+	    <div class="description">
+            ${place.description}
+            </div>
+                     </article>`;
+          $('section.places').append(template);
+	    }
+	  });
+	};
 
 	// search places
 	$(".filters button").bind("click", searchPlace);
-	searchPlace();
+	//searchPlace();
 });
